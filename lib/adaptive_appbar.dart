173977b63ce1288e51f582ada1e6fc4c28670405
ziadhassan7 <ziadhassan7@gmail.com>
2023-12-14 03:56:40 +0200

    Add custom widget to appbar

--- lib/adaptive_appbar.dart
+++ lib/adaptive_appbar.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 //ignore: must_be_immutable
 class AdaptiveAppBar extends StatelessWidget implements PreferredSizeWidget {
   final BuildContext context;
-  // AppBar's title
+  /// AppBar's title
   final String title;
-  // Title for back button (Shows only on big screens)
+  /// Title for back button (Shows only on big screens)
   final String backButtonTitle;
-  // Handle on back button press
+  /// Handle on back button press
   final Function()? onBackPressed;
-  // Custom background color
+  /// Custom background color
   Color? backgroundColor;
-  // Custom foreground color
+  /// Custom foreground color
   Color foregroundColor;
-  // Custom widget at the end of the AppBar
+  /// Custom widget at the end of the AppBar
   Widget? widget;
 
   AdaptiveAppBar(
